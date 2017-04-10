5d6f64c109c2b86fcd6969754cb46b6c70198a60
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2017-04-10 14:25:07 -0300

    Add initial files

---
+++ app/src/main/java/br/pro/hashi/ensino/desagil/morse/MorseTree.java
@@ -0,0 +1,7 @@
+package br.pro.hashi.ensino.desagil.morse;
+
+public class MorseTree {
+    public char translate(String code) {
+        return '?';
+    }
+}
---
+++ app/src/test/java/br/pro/hashi/ensino/desagil/morse/MorseTreeTest.java
@@ -0,0 +1,15 @@
+package br.pro.hashi.ensino.desagil.morse;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MorseTreeTest {
+    private MorseTree tree;
+
+    @Before
+    public void setUp() {
+        tree = new MorseTree();
+    }
+}
