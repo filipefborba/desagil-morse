1984f60ab30879ebe4c563923f2238cf22da52e9
moreira <gabriel.moreira98@hotmail.com>
2017-04-15 15:40:29 -0300

    add buttonlistener

--- app/src/main/java/br/pro/hashi/ensino/desagil/morse/SendActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/morse/SendActivity.java
@@ -1,15 +1,26 @@
 package br.pro.hashi.ensino.desagil.morse;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MenuInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.Toast;
 
+import static br.pro.hashi.ensino.desagil.morse.R.id.button;
+import static br.pro.hashi.ensino.desagil.morse.R.id.button4;
 
-public class SendActivity extends AppCompatActivity {
+
+public class SendActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText numberEdit;
     private EditText messageEdit;
 
@@ -20,8 +31,21 @@ public class SendActivity extends AppCompatActivity {
         setContentView(R.layout.activity_send);
 
         messageEdit = (EditText) findViewById(R.id.messageEdit);
-        numberEdit = (EditText) findViewById(R.id.numberEdit);
+
+
+        Button button_ready = (Button) findViewById(R.id.button4);
+        button_ready.setOnClickListener(this);
+
+
     }
+
+    public void onClick(View v) {
+        Toast.makeText(this, "Clicked on Button", Toast.LENGTH_LONG).show();
+
+        // Sua Ação
+
+    }
+
 
 
     public void sendMessage(View view) {
@@ -35,9 +59,10 @@ public class SendActivity extends AppCompatActivity {
 
             Toast toast = Toast.makeText(this, "Message sent to number!", Toast.LENGTH_SHORT);
             toast.show();
-        }
-        catch(IllegalArgumentException exception) {
+        } catch (IllegalArgumentException exception) {
             Log.e("SendActivity", "number or message empty");
         }
     }
+
+
 }
