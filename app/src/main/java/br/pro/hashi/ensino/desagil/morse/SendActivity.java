4cbb096dac0a0efab14134892fa5dde9af88f91f
moreira <gabriel.moreira98@hotmail.com>
2017-04-18 15:31:36 -0300

    long touch and short touch implemented

--- app/src/main/java/br/pro/hashi/ensino/desagil/morse/SendActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/morse/SendActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -12,7 +13,7 @@ import android.widget.ScrollView;
 import android.widget.Toast;
 
 
-public class SendActivity extends AppCompatActivity implements View.OnClickListener {
+public class SendActivity extends AppCompatActivity implements View.OnClickListener, View.OnTouchListener {
 
     private EditText numberEdit;
     private EditText messageEdit;
@@ -26,6 +27,9 @@ public class SendActivity extends AppCompatActivity implements View.OnClickListe
     public ImageButton button_ready;
     public String caregiver_number;
     private boolean turn;
+    private long startTime;
+    private long endTime;
+    private long time;
 
 
     @Override
@@ -43,12 +47,14 @@ public class SendActivity extends AppCompatActivity implements View.OnClickListe
         button_ready = (ImageButton) findViewById(R.id.readytext);
         morse = (ImageButton) findViewById(R.id.morse);
         button_ready.setOnClickListener((View.OnClickListener) this);
+        morse.setOnTouchListener((View.OnTouchListener) this);
         //botoes de mensagens prontas
+
         button2 = (Button) findViewById(R.id.button2);
-        button3= (Button) findViewById(R.id.button3);
-        button4= (Button) findViewById(R.id.button4);
-        button5= (Button) findViewById(R.id.button5);
-        button6= (Button) findViewById(R.id.button6);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+        button5 = (Button) findViewById(R.id.button5);
+        button6 = (Button) findViewById(R.id.button6);
         button6.setOnClickListener((View.OnClickListener) this);
         button5.setOnClickListener((View.OnClickListener) this);
         button4.setOnClickListener((View.OnClickListener) this);
@@ -58,19 +64,19 @@ public class SendActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
+
     public void onClick(View v) {
-        if (turn == false){
+        if (turn == false) {
             ready.setVisibility(View.VISIBLE);
             morse.setVisibility(View.INVISIBLE);
             turn = true;
-        }
-        else{
+        } else {
             ready.setVisibility(View.INVISIBLE);
             morse.setVisibility(View.VISIBLE);
             turn = false;
         }
         switch (v.getId()) {
-            case  R.id.button2: {
+            case R.id.button2: {
                 String text_message = button2.getText().toString();
                 String text_number = caregiver_number;
                 ;
@@ -82,7 +88,7 @@ public class SendActivity extends AppCompatActivity implements View.OnClickListe
                 // do something for button 2 click
                 break;
             }
-            case  R.id.button3: {
+            case R.id.button3: {
                 String text_message = button3.getText().toString();
                 String text_number = caregiver_number;
                 ;
@@ -96,7 +102,7 @@ public class SendActivity extends AppCompatActivity implements View.OnClickListe
 
 
             }
-            case  R.id.button4: {
+            case R.id.button4: {
                 String text_message = button4.getText().toString();
                 String text_number = caregiver_number;
                 ;
@@ -110,7 +116,7 @@ public class SendActivity extends AppCompatActivity implements View.OnClickListe
 
 
             }
-            case  R.id.button5: {
+            case R.id.button5: {
                 String text_message = button5.getText().toString();
                 String text_number = caregiver_number;
                 ;
@@ -124,7 +130,7 @@ public class SendActivity extends AppCompatActivity implements View.OnClickListe
 
 
             }
-            case  R.id.button6: {
+            case R.id.button6: {
                 String text_message = button6.getText().toString();
                 String text_number = caregiver_number;
                 ;
@@ -139,8 +145,9 @@ public class SendActivity extends AppCompatActivity implements View.OnClickListe
             }
 
 
+        }
     }
-    }
+
 
     public void sendMessage(View view) {
         SmsManager manager = SmsManager.getDefault();
@@ -157,9 +164,41 @@ public class SendActivity extends AppCompatActivity implements View.OnClickListe
 
             messageEdit.setText("");
             numberEdit.setText("");
-        }
-        catch(IllegalArgumentException exception) {
+        } catch (IllegalArgumentException exception) {
             Log.e("SendActivity", "number or message empty");
         }
     }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            //record the start time
+            startTime = event.getEventTime();
+
+            //System.out.println("LC  IN DOWN");
+        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+            //record the end time
+            endTime = event.getEventTime();
+            //System.out.println("LC   IN UP");
+        }
+        if (endTime - startTime > 0) {
+            time = endTime - startTime;
+            System.out.println(time);
+            if (time <= 150){
+                messageEdit.append(".");
+            }
+            else if (time > 150){
+                messageEdit.append("-");
+            }
+        }
+        else if (endTime - startTime < 0){
+            time = startTime - endTime;
+            if (time > 700){
+                messageEdit.append(" ");
+            }
+
+        }
+        return false;
+    }
+
 }
