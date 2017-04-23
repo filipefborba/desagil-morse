0a0c0ab541f334a0f3a9ae73a2db4af5ac7b2923
Filipe Foschini Borba <filipefborba@gmail.com>
2017-04-23 01:24:57 -0300

    Fixed tree creation; added backspace function for message; fixed node class

--- app/src/main/java/br/pro/hashi/ensino/desagil/morse/MorseTree.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/morse/MorseTree.java
@@ -8,8 +8,8 @@ import java.util.List;
 public class MorseTree {
     private SendActivity send;
     private String letter;
-    public Node[] nodes;
-    private String[] tree_to_morse = {null,"e","t","i","a","n","m","s","u","r","w","d","k","g","o",
+    private Node[] nodes;
+    private String[] treeToMorse = {null,"e","t","i","a","n","m","s","u","r","w","d","k","g","o",
                                         "h","v","f",null,"l",null,"p","j","b","x","c","y","z","q",
                                         null,null,"5","4",null,"3","é",null,null,"2",null,null,
                                         "+",null,null,"à",null,"1","6","=","/",null,"ç",null,null,
@@ -24,54 +24,22 @@ public class MorseTree {
     public MorseTree() {
         this.nodes = new Node[126];
         createNodes();
-
     }
-
-    public String translate(List<String> code) {
-        Node actual_node = nodes[0];
-        String character = "";
-        for (int i = 0; i < code.size(); i++) {
-            String node_finder = code.get(i);
-            if (node_finder.equals(".")) {
-                if (actual_node.getLeft() != null) {
-                    actual_node = actual_node.getLeft();
-                }
-                else {
-                    actual_node.getValue();
-                }
-            }
-
-            if (node_finder.equals("-")) {
-                if (actual_node.getRight() != null) {
-                    actual_node = actual_node.getRight();
-                }
-                else {
-                    actual_node.getValue();
-                }
-            }
-
-            else {
-                character = "nao foi";
-            }
-        }
-        return character;
-    }
-
-
 
     public void createNodes() {
         int i;
 
-
-        for(i = nodes.length - 1; i >= nodes.length / 2; i--) {
-            nodes[i] = new Node(i, null, null);
+        Node[] nodes = new Node[127];
+        for (i = 0; i < treeToMorse.length; i++){
+            nodes[i] = new Node(treeToMorse[i], null, null);
         }
 
         for(i = nodes.length / 2 - 1; i >= 0; i--) {
-            nodes[i] = new Node(i, nodes[2 * i + 1], nodes[2 * i + 2]);
+            nodes[i] = new Node(treeToMorse[i], nodes[2 * i + 1], nodes[2 * i + 2]);
         }
+    }
 
-
-
+    public String translate(List<String> morse){
+        return "?";
     }
 }
--- app/src/main/java/br/pro/hashi/ensino/desagil/morse/Node.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/morse/Node.java
@@ -3,13 +3,13 @@ package br.pro.hashi.ensino.desagil.morse;
 
 public class Node {
     private boolean open;
-    private int value;
+    private String character;
     private Node left;
     private Node right;
 
-    public Node(int value, Node left, Node right) {
+    public Node(String character, Node left, Node right) {
         open = true;
-        this.value = value;
+        this.character = character;
         this.left = left;
         this.right = right;
     }
@@ -21,7 +21,7 @@ public class Node {
         if(right != null) {
             right.print();
         }
-        System.out.print(" " + value);
+        System.out.print(" " + character);
     }
 
 
@@ -31,8 +31,8 @@ public class Node {
     public void setOpen(boolean open) {
         this.open = open;
     }
-    public int getValue() {
-        return value;
+    public String getCharacter() {
+        return character;
     }
     public Node getLeft() {
         return left;
